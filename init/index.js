3e4129a0d84d319bae8fd4657000e042486c97c1
Vedant Sharma <thisisvadantsharma.05@gmail.com>
2026-07-30 13:08:52 +0530

    style: add styling for index, show, and edit pages

--- init/index.js
+++ init/index.js
@@ -1,27 +1,25 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing  = require("../models/listing.js");
+const Listing = require("../models/listing.js");
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-const URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(URL);
-};
-
-
-main().then((res) => {
-    console.log("Connectes to DB");
-}) .catch((err) => {
-    console.log(err);
-});
-
+  await mongoose.connect(MONGO_URL);
+}
 
 const initDB = async () => {
-    await Listing.deleteMany({}); 
-    await Listing.insertMany(initData.data);
-    console.log("Data is initialize");
-
-}
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
 
 initDB(); 
--- models/listing.js
+++ models/listing.js
@@ -1,29 +1,24 @@
 const mongoose = require("mongoose");
-const schema = mongoose.Schema;
- 
+const Schema = mongoose.Schema;
 
-const listingSchema = schema({
-    title: {
-        type: String,
-        required: true, 
-    },
-    description: String,
-    image: {
-        filename: {
-            type: String,
-            default: "listingimage",
-        },
-        url: {
-            type: String,
-            default:
-                "https://media.istockphoto.com/id/1087673356/photo/highway-at-sunrise-going-into-death-valley-national-park.jpg?s=1024x1024&w=is&k=20&c=djFLS8h7JpVqpY4MLtnHInoKygtftaUcwR03prsKuss=",
-        },
-    },
-    price: Number,
-    location: String,
-    country: String
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-
-module.exports = Listing; 
+module.exports = Listing;   
